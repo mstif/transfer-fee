142aad9d617e7ea1e6d7feadfe5ff17238f6fa5a
mstisf <mstisf@gmail.com>
2022-03-13 23:48:04 +0300

    Rename variables

--- src/main/kotlin/ru/netology/TransferFee.kt
+++ src/main/kotlin/ru/netology/TransferFee.kt
@@ -2,8 +2,8 @@ package ru.netology
 
 import kotlin.math.roundToInt
 
-const val percentFee = 0.75
-const val minFee = 35 * 100
+const val PERCENT_FEE = 0.75
+const val MIN_FEE = 35 * 100
 fun main() {
     val amount = 4550 * 100
     println("Комиссия с перевода $amount копеек составит ${getTransferFee(amount)} копеек")
@@ -12,6 +12,6 @@ fun main() {
 
 fun getTransferFee(amount: Int): Int {
 
-    val fee = (amount * percentFee / 100).roundToInt()
-    return fee.coerceAtLeast(minFee)
+    val fee = (amount * PERCENT_FEE / 100).roundToInt()
+    return fee.coerceAtLeast(MIN_FEE)
 }
